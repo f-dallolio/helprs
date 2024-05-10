739cce79ae953d5f5cd408951549c3abfd5ffc54
Filippo DallOlio <fdallolio@brocku.ca>
2024-05-10 19:18:53 -0400

    .

--- R/call_add_ns.R
+++ R/call_add_ns.R
@@ -6,23 +6,40 @@
 #' @export
 #'
 # #'  @examples
-call_add_ns <- function(x, ..., .ns = NULL, .fn = NULL){
+call_add_ns <- function(x, ..., .fn = NULL, .ns = NULL, .private = NULL){
   x <- encall0(x)
 
   if(is.null(.ns)){
-
     if(is.null(.fn)){
       .fn <- try(eval(x[[1]], envir = globalenv()), silent = TRUE)
     }
     if (is.function(.fn)){
-      out <- rlang:::call_add_namespace(x, .fn)
-      return(out)
+      .ns <- ns_env_name(.fn)
     } else {
       warning("Could not find a namespace. Returning the input as call.")
       return(x)
     }
+  } else if (is.environment(.ns)) {
+    .ns <- ns_env_name(.ns)
   } else {
-    call_replace(x, .ns = .ns)
+    warning("Could not find a namespace. Returning the input as call.")
+    return(x)
   }
+
+  if(is_string(.ns)){
+    .ns <- str2lang(.ns)
+  }
+  .fn <- str2lang(call_name(x))
+
+  if(.private){
+    op <- ":::"
+  } else {
+    op <- "::"
+  }
+
+  x0 <- as.list(x)
+  x0[[1]] <- call(op, .ns, .fn)
+
+  as.call(x0)
 }
 
